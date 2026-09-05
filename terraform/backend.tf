db916663ae3f84465f2d28c891fab017dd4f019e
Konrad <k.szpytma3@gmail.com>
2026-09-05 19:35:32 +0200

    feat: tf action

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,2 +1,9 @@
-# Backend intentionally omitted for local state.
-# State file will be: terraform.tfstate in the terraform/ directory
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-thepawject-tfstate"
+    storage_account_name = "thepawjecttfstate"
+    container_name       = "tfstate"
+    key                  = "thepawject.tfstate"
+    use_azuread_auth     = true
+  }
+}
